bd1ce1016402b2ede20ee2e9119fcec44c32afa8
JamesOk3 <jameskambanga@gmail.com>
2023-09-30 11:45:17 +0100

    addition method added

---
+++ src/HellowWorld.java
@@ -0,0 +1,15 @@
+public class HellowWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi, Am James Ok");
+        System.out.println("Version Control Example");
+
+        System.out.println("tWo numbers addition");
+
+        System.out.println(addition(250, 300));
+    }
+
+    public static int addition (int a, int b) {
+        // A method to add two numbers
+        return a + b;
+    }
+}
